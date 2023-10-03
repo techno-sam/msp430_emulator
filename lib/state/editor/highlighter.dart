4e90309de6bf56f51d23061d0f0d9d6a988ff130
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-10-03 08:05:40 -0700

    copy copy copyright

--- lib/state/editor/highlighter.dart
+++ lib/state/editor/highlighter.dart
@@ -1,7 +1,19 @@
 /*
- * MIT License
+ * Copyright (c) 2023 Sam Wagenaar
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- * Copyright (c) 2022-2023 icedman, Sam Wagenaar
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2022 icedman
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
